97c0e847c2130069a60d7f4ab62b5f8c2c8163ba
Ankhbayar <antony.baasan@gmail.com>
2018-07-14 22:46:38 -0400

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://35.190.83.210/',
+  backendUrl: 'https://35.190.83.210',
   receiptDetectorFunctionUrl: 'https://us-central1-bookingpreneur.cloudfunctions.net/receiptdetector'
 };
